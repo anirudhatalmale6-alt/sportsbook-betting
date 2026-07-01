f0b754d1ff7047b8af6272fc9e371b982c44f031
Anirudha Talmale <anirudhatalmale6-alt@users.noreply.github.com>
2026-07-01 22:51:50 +0000

    Lighten color scheme to match Jilbet dark blue-gray tones

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <meta name="theme-color" content="#111111">
+    <meta name="theme-color" content="#14181e">
     <meta name="description" content="BetZone - Premium Sports Betting & Casino">
 
     <title>{{ config('app.name', 'BetZone') }} - Sports Betting & Casino</title>
